79b789c0307ed2debdc260a32d80860f82b87fa1
Om Ambarkar <heyom7@gmail.com>
2022-01-24 13:43:13 +0530

    superfirst commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Home from './webpages/home/home.jsx';
+import Aboutme from './webpages/aboutMe/aboutme';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <Aboutme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/webpages/aboutMe/aboutme.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Aboutme() {
+    return <div className='AboutMe'>
+        <h1>hii</h1>
+    </div>;
+}
